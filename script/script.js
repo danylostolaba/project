8bcdc28be868f20d1887962b3812b9b329caae49
danylostolaba <danylo.stolaba-pi2203@kep.nung.edu.ua>
2025-11-22 11:00:19 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let modal = document.getElementById("contactModal");
+let btn = document.getElementById("contactBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    return false; 
+};
